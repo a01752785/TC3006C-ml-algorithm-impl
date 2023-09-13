c3dde936a3ade830121b5a6e95727bf2b825a8ad
a01752785 <a01752785@tec.mx>
2023-09-12 23:59:45 -0600

    Adding multiple executions

--- california_clustering_main.py
+++ california_clustering_main.py
@@ -2,52 +2,55 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import random
-from sklearn import cluster
+from sklearn import cluster as sklearn_cluster
 from sklearn.preprocessing import StandardScaler
 
 from k_means import Cluster, KMeans, PandasDataFrameAdapter
 
 if __name__ == "__main__":
-    california: pd.DataFrame = pd.read_csv("california_housing.csv")
+    
 
-    # Cluster using longitude, latitude and housing_median_age
-    columns_to_keep: list[str] = ["longitude", "latitude", "median_income"]
-    columns_to_drop: list[str] = [
-        col for col in california.columns if col not in columns_to_keep
-    ]
-    california = california.drop(columns=columns_to_drop)
+    for i in range(5):
+        california: pd.DataFrame = pd.read_csv("california_housing.csv")
 
-    # Scale data
-    scaler = StandardScaler()
-    normalized_data = scaler.fit_transform(california)
-    california = pd.DataFrame(normalized_data, columns=columns_to_keep)
+        # Cluster using longitude, latitude and housing_median_age
+        columns_to_keep: list[str] = ["longitude", "latitude", "median_income"]
+        columns_to_drop: list[str] = [
+            col for col in california.columns if col not in columns_to_keep
+        ]
+        california = california.drop(columns=columns_to_drop)
 
-    california_2 = california.copy(deep=True)
+        # Scale data
+        scaler = StandardScaler()
+        normalized_data = scaler.fit_transform(california)
+        california = pd.DataFrame(normalized_data, columns=columns_to_keep)
 
-    # Scikit-learn implementation
-    sklearn_k_means = cluster.KMeans(n_clusters=3)
-    california["cluster"] = sklearn_k_means.fit_predict(california)
-    california["source"] = "sklearn"
+        california_2 = california.copy(deep=True)
+        
+        # Scikit-learn implementation
+        sklearn_k_means = sklearn_cluster.KMeans(n_clusters=3)
+        california["cluster"] = sklearn_k_means.fit_predict(california)
+        california["source"] = "sklearn"
 
-    # Own implementation
-    adapter = PandasDataFrameAdapter(california_2)
-    k_means = KMeans(data=adapter.points(), k=3)
-    k_means.train(epochs=10)
-    for cluster in k_means.clusters():
-        print(cluster.center())
-    california_2["cluster"] = k_means.list_of_cluster_nums()
-    california_2["source"] = "own impl"
+        # Own implementation
+        adapter = PandasDataFrameAdapter(california_2)
+        k_means = KMeans(data=adapter.points(), k=3)
+        k_means.train(epochs=10)
+        for cluster in k_means.clusters():
+            print(cluster.center())
+        california_2["cluster"] = k_means.list_of_cluster_nums()
+        california_2["source"] = f"own impl run {i}"
 
-    # Data visualization
-    california_combined: pd.DataFrame = pd.concat([california, california_2])
-    california_combined["cluster"] = california_combined["cluster"].astype(
-        "category"
-    )
-    sns.relplot(x="longitude", y="latitude",
-                hue="cluster", data=california_combined, col="source")
-    plt.show()
-    plt.plot(k_means.sse_epoch_list())
-    plt.title("Error over time. Source: Own impl")
-    plt.xlabel("Epochs")
-    plt.ylabel("Sum of squared error")
-    plt.show()
+        # Data visualization
+        california_combined: pd.DataFrame = pd.concat([california, california_2])
+        california_combined["cluster"] = california_combined["cluster"].astype(
+            "category"
+        )
+        sns.relplot(x="longitude", y="latitude",
+                    hue="cluster", data=california_combined, col="source")
+        plt.show()
+        plt.plot(k_means.sse_epoch_list())
+        plt.title(f"Error over time. Source: Own impl execution {i}")
+        plt.xlabel("Epochs")
+        plt.ylabel("Sum of squared error")
+        plt.show()
